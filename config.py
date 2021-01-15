6ec32bef46cf56908cb8d70e0bf1f5b8bb739fcb
Kracha0907 <73388987+Kracha0907@users.noreply.github.com>
2021-01-15 16:36:11 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
-
+# Параметры путей до папок
 game_folder = os.path.dirname(__file__)
 sprites_folder = os.path.join(game_folder, "sprites")
 scenes_folder = os.path.join(game_folder, "scenes")
 map_folder = os.path.join(scenes_folder, "maps")
 
-# game options
-WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 1000, 700
-FPS = 10
+# Параметры игры
+WINDOW_SIZE = WINDOW_WIDTH, WINDOW_HEIGHT = 1300, 700
+FPS = 8
 MAPS_DIR = 'maps'
-TILE_SIZE = 50
+TILE_SIZE = 40
 
-# define colors
+# Цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
