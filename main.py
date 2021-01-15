d047efb9ee7046569b006437bace0b3c971549ab
Kracha0907 <73388987+Kracha0907@users.noreply.github.com>
2021-01-15 03:33:03 +0300

    Update main.py
    Добавлена камера

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
+import os
 
-from config import WINDOW_SIZE, FPS
+from config import WINDOW_SIZE, FPS, sprites_folder, TILE_SIZE, WINDOW_WIDTH, WINDOW_HEIGHT
 from game import Game
 from ghost import Ghost
 from labyrinth import Labyrinth
@@ -9,12 +10,31 @@ from energy_life import EnergyLife
 from artifacts_rend import ArtifactsRender
 
 
+class Camera:
+    # зададим начальный сдвиг камеры
+    def __init__(self):
+        self.dx = 0
+        self.dy = 0
+
+    # сдвинуть объект obj на смещение камеры
+    def apply(self, obj):
+        obj.center[0] += self.dx
+        obj.center[1] += self.dy
+
+    # позиционировать камеру на объекте target
+    def update(self, target):
+        self.dx = -(target.center[0] + TILE_SIZE // 2 - WINDOW_WIDTH // 2)
+        self.dy = -(target.center[1] + TILE_SIZE // 2 - WINDOW_HEIGHT // 2)
+
+
 class GameLevel:
     def __init__(self, pacman_coords, ghost_coords, artifacts_count, name_map):
         self.pacman_coords = pacman_coords
         self.ghost_coords = ghost_coords
         self.artifacts_count = artifacts_count
         self.name_map = name_map
+        self.background = pygame.image.load(os.path.join(sprites_folder, "back.jpg"))
+        self.background = pygame.transform.scale(self.background, WINDOW_SIZE)
 
     def main(self):
         pygame.init()
@@ -26,6 +46,7 @@ class GameLevel:
         self.pacman.artifacts = self.artifacts_count
         ghost = Ghost(self.ghost_coords, labyrinth, all_sprites)
         game = Game(labyrinth, self.pacman, ghost)
+        camera = Camera()
         energy_life = EnergyLife(screen, self.pacman)
         artifacts_rend = ArtifactsRender(screen, 0)
 
@@ -54,9 +75,15 @@ class GameLevel:
             if labyrinth.map[rect_pacman[1]][rect_pacman[0]] == 'A' and self.pacman.energy < 100:
                 self.pacman.artifacts += 1
                 labyrinth.map[rect_pacman[1]][rect_pacman[0]] = '0'
+            screen.blit(self.background, (0, 0))
             self.pacman.update_hero()
             ghost.update_hero()
-            screen.fill((0, 0, 0))
+            # изменяем ракурс камеры
+            camera.update(self.pacman)
+            # обновляем положение всех спрайтов
+            for sprite in all_sprites:
+                print(sprite)
+                camera.apply(sprite)
             game.render(screen)
             artifacts_rend.render(self.pacman.artifacts)
             energy_life.render(self.pacman)
