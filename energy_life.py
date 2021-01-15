19566fe80768748bc0c9afb1f35ed44670b75b05
Kracha0907 <73388987+Kracha0907@users.noreply.github.com>
2021-01-15 16:36:33 +0300

    Update energy_life.py

--- energy_life.py
+++ energy_life.py
@@ -1,4 +1,6 @@
 import pygame
+# Полоса энергии и жизни
+
 
 class EnergyLife:
     def __init__(self, screen, p):
@@ -12,11 +14,13 @@ class EnergyLife:
         x = 1100
         y = 10
         color = pygame.Color('black')
+        # Отображение полосы энергии
         color.hsva = (190, 100, 100, 100)
         pygame.draw.rect(self.screen, color, ((x, y), (1.5 * self.energy, 20)))
         color.hsva = (190, 100, 75, 100)
         pygame.draw.rect(self.screen, color, ((x - 1, y - 1), (1.5 * 100 + 2, 21)), 5)
         y = 50
+        # Отображение полосы жизни
         color.hsva = (0, 100, 100, 100)
         pygame.draw.rect(self.screen, color, ((x, y), (1.5 * self.life, 20)))
         color.hsva = (0, 100, 75, 100)
